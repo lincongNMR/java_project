778a26d240aa305a1e7defa375f55a8ab0739fa7
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-02 20:11:54 -0500

    Eclipse gives me a charlotte :laughing:
    Just a void line seprator. It is not possible to pass a line seprator.

--- src/org/sourcejav/casejava/main/ShowJava.java
+++ src/org/sourcejav/casejava/main/ShowJava.java
@@ -25,7 +25,7 @@ public class ShowJava {
 		String buffer="vague";
 		if (bufferVec.add(buffer) )
 			buffer.concat(bufferVec.toString() );
-		System.out.println("Buffer at the new separator\nand the buffer is: "+buffer);
+		System.out.println("Buffer at the new void line separator\nand the buffer is: "+buffer);
 		Map<String, Vector<String>> map = new HashMap<String, Vector<String>>();
 	  Vector<String> vec = new Vector<String>();
 	  vec.add("This is a example");
