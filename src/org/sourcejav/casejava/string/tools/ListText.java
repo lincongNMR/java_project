8400b0279cb877e0a975a327bffa5ad28bf12b71
Zixuan Wang <wzixuanCC@outlook.com>
2018-02-02 19:29:46 -0500

    Added initialize string.

--- src/org/sourcejav/casejava/string/tools/ListText.java
+++ src/org/sourcejav/casejava/string/tools/ListText.java
@@ -5,6 +5,12 @@ public class ListText {
 	public ListText() {
 		// TODO Auto-generated constructor stub
 		System.out.format("string", null);
+		initialization("just a string");
+	}
+	public static initialization(String traditional){
+		System.out.println("In initialization\nat the same time");
+		String init="string";
+		init.concat(traditional);
 	}
 
 }
